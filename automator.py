338696be8adec202a92c4e883bcdd99576c1300d
Vincent <vincentwarmerdam@gmail.com>
2020-03-13 23:14:23 +0100

    workwork

---
+++ automator.py
@@ -0,0 +1,39 @@
+import json
+import pathlib
+
+import yaml
+import click
+
+
+@click.group()
+def main():
+    """This cli is oke, okeh?"""
+    pass
+
+
+@click.command()
+def to_json():
+    """It's easy being green."""
+    d = yaml.load(pathlib.Path("ideas.yml").read_text(), Loader=yaml.FullLoader)
+    print(json.dumps(d))
+
+
+@click.command()
+def run_jinja():
+    """It's not easy being red."""
+    from jinja2 import Environment, select_autoescape, FileSystemLoader
+    d = yaml.load(pathlib.Path("ideas.yml").read_text(), Loader=yaml.FullLoader)
+    print(d)
+    env = Environment(
+        loader=FileSystemLoader('src'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    with open('public/index.html', 'w') as f:
+        f.write(env.get_template('index.html').render(ideas=d))
+
+
+main.add_command(run_jinja)
+
+
+if __name__ == "__main__":
+    main()
