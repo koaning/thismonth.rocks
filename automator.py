2a44f0b8fd5d7692267be9761a301a74cab85665
Vincent <vincentwarmerdam@gmail.com>
2020-03-22 11:14:35 +0100

    redesign

--- automator.py
+++ automator.py
@@ -20,22 +20,40 @@ def to_json():
     pathlib.Path("public/tips.json").write_text(json.dumps(d))
 
 
+def handle_images():
+    for p in pathlib.Path("src/img/icons/").glob("*"):
+        p_towards = f"public/img/icons/{p.parts[-1]}"
+        print(f"moving {p} -> {p_towards}")
+        copyfile(p, pathlib.Path(p_towards))
+    for p in pathlib.Path("src/img").glob("*.png"):
+        p_towards = f"public/img/{p.parts[-1]}"
+        print(f"moving {p} -> {p_towards}")
+        copyfile(p, pathlib.Path(p_towards))
+
+def handle_category_pages(env, ideas):
+    flatten = lambda l: [item for sublist in l for item in sublist]
+    uniq_tags = set(flatten([d['tags'] for d in ideas]))
+    
+    for tag in uniq_tags:
+        idea_subset = [d for d in ideas if tag in d['tags']]
+        with open(f'public/{tag}.html', 'w') as f:
+            f.write(env.get_template('index.html').render(ideas=idea_subset, tag=tag))
+            click.echo("created page: " + click.style(f"public/{tag}.html", "blue"))
+
 @click.command()
 def run_jinja():
     """It's not easy being red."""
     glob = pathlib.Path("src/ideas").glob("*.yml")
-    d = [yaml.load(p.read_text(), Loader=yaml.FullLoader) for p in glob]
+    ideas = [yaml.load(p.read_text(), Loader=yaml.FullLoader) for p in glob]
     env = Environment(
         loader=FileSystemLoader('src'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     with open('public/index.html', 'w') as f:
-        f.write(env.get_template('index.html').render(ideas=d))
+        f.write(env.get_template('index.html').render(ideas=ideas))
+    handle_images()
+    handle_category_pages(env, ideas=ideas)
     
-    for p in pathlib.Path("src/img").glob("*.svg"):
-        copyfile(p, pathlib.Path(f"public/img/{p.parts[-1]}"))
-    for p in pathlib.Path("src/img").glob("*.png"):
-        copyfile(p, pathlib.Path(f"public/img/{p.parts[-1]}"))
 
 
 main.add_command(run_jinja)
